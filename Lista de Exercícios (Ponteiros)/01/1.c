f6ce15b3fac982435cd20a58a03f79578ca5d412
YZhLu <luiz.zito@icloud.com>
2021-06-14 11:51:30 -0300

    Update 1.c

--- Lista de Exercícios (Ponteiros)/01/1.c
+++ Lista de Exercícios (Ponteiros)/01/1.c
@@ -13,6 +13,6 @@ int main() {
     printf("A expressão `*p - *q` retorna o valor `%d`\n",(*p - *q));
     printf("A expressão `**&p` retorna o valor `%d`\n",(**&p));
     printf("A expressão `3 - *p/(*q) + 7` retorna o valor `%d`\n",(3 - *p/(*q) + 7));
-     
+    
     return 0;
 }
