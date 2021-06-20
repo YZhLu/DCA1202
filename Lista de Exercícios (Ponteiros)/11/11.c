05e9869b1d2657013b2e9feeb0d5d5ba606b726d
YZhLu <luiz.zito@icloud.com>
2021-06-20 20:09:04 -0300

    Update question 11

--- Lista de Exercícios (Ponteiros)/11/11.c
+++ Lista de Exercícios (Ponteiros)/11/11.c
@@ -4,14 +4,15 @@ int main()
 {
     float aloha[10], coisas[10][5], *pf, value = 2.2;
     int i = 3;
-
+   
     aloha[2] = value;
     scanf("%f", &aloha);
-    //aloha = value ";
+    //aloha = value;
     printf("%f", aloha);
     coisas[4][4] = aloha[3];
     //coisas[5] = aloha;
     //pf = value;
     pf = aloha;
+    
     return 0;
 }
