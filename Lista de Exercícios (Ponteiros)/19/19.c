eb3818a2554748870f8a78885b813a5e4ce4920a
YZhLu <luiz.zito@icloud.com>
2021-06-25 11:32:57 -0300

    Update 19.c

--- Lista de Exercícios (Ponteiros)/19/19.c
+++ Lista de Exercícios (Ponteiros)/19/19.c
@@ -2,6 +2,27 @@
 #include <assert.h>
 #include <stdio.h>
 
+/*
+
+Chame GC_MALLOC sempre que quiser chamar malloc. 
+Isso inicializa a memória para zero como calloc; 
+não há necessidade de limpar explicitamente o resultado.
+Se você souber que um objeto não conterá ponteiros para 
+o heap coletado pelo lixo e não precisar ser inicializado,
+chame GC_MALLOC_ATOMIC em vez disso.
+
+Uma função GC_FREE é fornecida, mas não precisa ser chamada.
+Para objetos muito pequenos, 
+seu programa provavelmente terá um desempenho melhor
+ se você não chamá-lo e deixar que o coletor faça seu trabalho.
+
+Uma função GC_REALLOC se comporta como o realloc da biblioteca C.
+Ele aloca memória livre de ponteiro não inicializada
+se o objeto original foi alocado dessa maneira.
+
+A seguir temos um exemplo:
+*/
+
 int main() {
   int i;
 
